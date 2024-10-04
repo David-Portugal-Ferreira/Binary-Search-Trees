2bb5e531b9b6e05e06873a94a8a7996e9c1f8075
David-Portugal-Ferreira <davidportugal44@gmail.com>
2024-10-04 10:02:38 +0100

    Make function to create balance binary search tree

---
+++ main.js
@@ -0,0 +1,5 @@
+const Tree = require("./tree")
+
+let tree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
+
+console.log(tree.arr)
--- tree.js
+++ tree.js
@@ -1,59 +1,72 @@
 const Node = require("./node.js");
 
 class Tree {
-  root = null;
-
   constructor(arr) {
-    this.arr = arr;
+    this.arr = buildTree(arr);
   }
 }
 
-function buildTree(dataArray = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]) {
-	let arr = [];
-	for(let i = 0; i < dataArray.length; i++) {
-		if(arr.includes(dataArray[i])) {
-			continue;
-		} else {
-			arr.push(dataArray[i]);
-		}
-	}
+function buildTree(dataArray) {
+  let arr = [];
+  for (let i = 0; i < dataArray.length; i++) {
+    if (arr.includes(dataArray[i])) {
+      continue;
+    } else {
+      arr.push(dataArray[i]);
+    }
+  }
 
-	arr = mergeSort(arr);
+  arr = mergeSort(arr);
+
+  console.log(arr);
+  let nodeArray = balanceBinaryTree(arr);
+
+  console.log(nodeArray);
 }
-
 
 function mergeSort(arr) {
-	if(arr.length === 1) return arr;
-	let mid = Math.floor(arr.length / 2);
+  if (arr.length === 1) return arr;
+  let mid = Math.floor(arr.length / 2);
 
-	let leftSide = mergeSort(arr.slice(0, mid))
-	let rigthSide = mergeSort(arr.slice(mid))
-	return merge(leftSide, rigthSide);
+  let leftSide = mergeSort(arr.slice(0, mid));
+  let rigthSide = mergeSort(arr.slice(mid));
+  return merge(leftSide, rigthSide);
 }
 function merge(left, rigth) {
-	let newArr = [];
-	let i = 0;
-	let j = 0;
-	let k = 0;
+  let newArr = [];
+  let i = 0;
+  let j = 0;
+  let k = 0;
 
-	while(left.length > i && rigth.length > j) {
-		if(left[i] < rigth[j]) {
-			newArr[k++] = left[i++];
-		} else {
-			newArr[k++] = rigth[j++]; 
-		}
-	}
+  while (left.length > i && rigth.length > j) {
+    if (left[i] < rigth[j]) {
+      newArr[k++] = left[i++];
+    } else {
+      newArr[k++] = rigth[j++];
+    }
+  }
 
-	while(left.length > i) {
-		newArr[k++] = left[i++]
-	}
+  while (left.length > i) {
+    newArr[k++] = left[i++];
+  }
 
-	while(rigth.length > j) {
-		newArr[k++] = rigth[j++]
-	}
+  while (rigth.length > j) {
+    newArr[k++] = rigth[j++];
+  }
 
-	return newArr
+  return newArr;
 }
 
+function balanceBinaryTree(arr) {
+	if(arr.length <= 0) return null;
+	let mid = Math.floor(arr.length / 2);
+	if(mid === 0) return new Node(arr);
 
-console.log(buildTree());
+	let node = new Node(arr[mid]);
+	node.left = balanceBinaryTree(arr.slice(0, mid));
+	node.right = balanceBinaryTree(arr.slice(mid + 1));
+
+	return node;
+}
+
+module.exports = Tree;
